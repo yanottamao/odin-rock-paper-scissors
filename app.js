059e93b2c774b06fc2ac7256f5939dcc9ea4ce35
Yanottama Oktabrian <oktatama4@gmail.com>
2024-02-16 16:05:27 +0700

    Add feature to play multiple rounds of game

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const choicesArray = ["Rock", "Paper", "Scissors"];
+playerWinCount = 0;
+computerWinCount = 0;
 
 function getComputerChoice() {
   const randomizer = Math.floor(Math.random() * choicesArray.length);
@@ -21,38 +23,52 @@ function getPlayerChoice() {
   }
 }
 
-function playGame() {
+function playRound() {
   const playerSelection = getPlayerChoice();
   const computerSelection = getComputerChoice();
   console.log(playerSelection);
   console.log(computerSelection);
 
   if (playerSelection == computerSelection)
-    console.log(
-      `It's DRAW - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
-  else if (playerSelection == "Rock" && computerSelection == "Paper")
-    console.log(
-      `Computer WINS - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
-  else if (playerSelection == "Rock" && computerSelection == "Scissors")
-    console.log(
-      `Player WINS - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
-  else if (playerSelection == "Paper" && computerSelection == "Rock")
-    console.log(
-      `Player WINS - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
-  else if (playerSelection == "Paper" && computerSelection == "Scissors")
-    console.log(
-      `Computer WINS - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
-  else if (playerSelection == "Scissors" && computerSelection == "Rock")
-    console.log(
-      `Computer WINS - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
-  else if (playerSelection == "Scissors" && computerSelection == "Paper")
-    console.log(
-      `Player WINS - Player : ${playerSelection} - Computer : ${computerSelection}`
-    );
+    return `It's DRAW - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  else if (playerSelection == "Rock" && computerSelection == "Paper") {
+    computerWinCount++;
+    return `Computer WINS - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+    playerWinCount++;
+    return `Player WINS - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+    playerWinCount++;
+    return `Player WINS - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+    computerWinCount++;
+    return `Computer WINS - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+    computerWinCount++;
+    return `Computer WINS - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+    playerWinCount++;
+    return `Player WINS - Player : ${playerSelection} - Computer : ${computerSelection}`;
+  }
+}
+
+function playGame() {
+  const playNumber = prompt("Enter playing times : (Number)");
+  console.log(playNumber);
+  if (!isNaN(playNumber)) {
+    console.log(playNumber);
+    for (let index = 0; index < playNumber; index++) {
+      playRound();
+    }
+    console.log(computerWinCount);
+    console.log(playerWinCount);
+    if (computerWinCount > playerWinCount) {
+      console.log("Computer Win the games");
+    } else if (computerWinCount < playerWinCount) {
+      console.log("Player Win the games");
+    } else console.log("It's DRAW");
+  } else {
+    console.log("NAN");
+    return playGame();
+  }
 }
