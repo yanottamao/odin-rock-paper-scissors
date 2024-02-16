917aa7d3ecf0264200bf6171fba5e1cb7c54f64f
Yanottama Oktabrian <oktatama4@gmail.com>
2024-02-16 15:00:52 +0700

    Add player choice

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const choicesArray = ["Rock", "Paper", "Scissors"];
+
 function getComputerChoice() {
-    const choicesArray = ["Rock","Paper","Scissors"]
-    const randomizer = Math.floor(Math.random() * choicesArray.length)
-    console.log(choicesArray[randomizer])
+  const randomizer = Math.floor(Math.random() * choicesArray.length);
+  return choicesArray[randomizer];
+}
+
+function getPlayerChoice() {
+  const playerSelectionPrompt = prompt("Enter your choice: ");
+  const firstLetter = playerSelectionPrompt.charAt(0).toUpperCase();
+  const restOfText = playerSelectionPrompt.substring(1).toLowerCase();
+  return firstLetter + restOfText;
 }
